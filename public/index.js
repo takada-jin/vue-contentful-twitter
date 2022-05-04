7a184f546fa7398965e08470e322439eb523a1d9
高田仁 <takadajin@takadahitoshinoMacBook-Pro.local>
2022-05-04 17:05:56 +0900

    ツイートできるところまで

---
+++ public/index.js
@@ -0,0 +1,43 @@
+const app = {
+    data() {
+      return {
+        form: {
+          tweetText: "",
+        },
+        maxTweetCharacter:140,
+        tweets: [],
+      };
+    },
+    computed: {
+      remainingTweetCharacter(){
+          return this.maxTweetCharacter - this.form.tweetText.length;
+      },
+      reversedTweets() {
+        return this.tweets.slice().reverse();
+      },
+    },
+    methods: {
+      tweet(){
+          this.tweets.push({
+            authorName: "高田 仁",
+            authorId: "@takada.jin",
+            tweetedAt: this.now(),
+            tweetText: this.form.tweetText,
+          });
+      },
+      now() {
+        const now = new Date();
+
+        const year = now.getFullYear();
+        const month = now.getMonth()+1;
+        const date = now.getDate();
+        const hour = now.getHours();
+        const min = now.getMinutes();
+        const sec = now.getSeconds();
+  
+        return `${year}/${month}/${date} ${hour}:${min}:${sec}`;
+      }
+    }
+  }
+  
+  Vue.createApp(app).mount('#app')
